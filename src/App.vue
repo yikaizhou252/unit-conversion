e3109e8e303bccb62692a8ce3d3f9c2c81b5a3ba
Yikai Zhou <zhou.yikai76@gmail.com>
2024-02-29 22:35:36 -0500

    new look + logo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  
-  <div class=" flex flex-col justify-center items-center text-center h-screen">
-    <h1 class=" font-heading justify-start font-bold text-8xl mb-2">Convert It!</h1>
+  <div class="flex flex-col justify-center items-center text-center h-screen">
+    <div class="flex flex-row align-baseline space-x-9">
+      <h1 class="font-heading justify-start font-bold text-8xl mb-2">Convert It!</h1>
+      <img class="object-fill h-20 w-20" src="./assets/icons/logo.png" />
+    </div>
     <DashboardView />
   </div>
 </template>
